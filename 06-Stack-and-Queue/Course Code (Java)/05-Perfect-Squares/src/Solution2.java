11123cbc4d64200d850b5de60e89d2e4b3d660e9
huyuansong <2455428161@qq.com>
2020-07-22 18:56:38 +0800

    维护了一些题目

--- 06-Stack-and-Queue/Course Code (Java)/05-Perfect-Squares/src/Solution2.java
+++ 06-Stack-and-Queue/Course Code (Java)/05-Perfect-Squares/src/Solution2.java
@@ -4,7 +4,20 @@ import javafx.util.Pair;
 // 279. Perfect Squares
 // https://leetcode.com/problems/perfect-squares/description/
 // 使用visited数组,记录每一个入队元素
-//
+/* 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+
+示例 1:
+
+输入: n = 12
+输出: 3 
+解释: 12 = 4 + 4 + 4.
+示例 2:
+
+输入: n = 13
+输出: 2
+解释: 13 = 4 + 9.
+
+ */
 // 时间复杂度: O(n)
 // 空间复杂度: O(n)
 public class Solution2 {
@@ -15,10 +28,10 @@ public class Solution2 {
         queue.addLast(new Pair<Integer, Integer>(n, 0));
 
         boolean[] visited = new boolean[n+1];  //由于是图结构，一个节点会有多种途径到达，因此存在很多的重复推入队列
-        visited[n] = true;  // n节点默认是访问过的
+        visited[n] = true;  // n节点上面已经加入队列了，默认是访问过的
 
         while(!queue.isEmpty()){
-            Pair<Integer, Integer> front = queue.removeFirst();
+            Pair<Integer, Integer> front = queue.removeFirst();  // 取出接下来的任务
             int num = front.getKey();
             int step = front.getValue();
 
@@ -27,8 +40,8 @@ public class Solution2 {
 
             for(int i = 1 ; num - i*i >= 0 ; i ++)
                 if(!visited[num - i * i]){  //该数字还没有被访问
-                    queue.addLast(new Pair(num - i * i, step + 1));
-                    visited[num - i * i] = true;   //该数字已经被访问过了
+                    queue.addLast(new Pair(num - i * i, step + 1));  // 修改步数
+                    visited[num - i * i] = true;   //标记该数字被访问过了
                 }
         }
 
--- 06-Stack-and-Queue/Course Code (Java)/05-Perfect-Squares/src/Solution3.java
+++ 06-Stack-and-Queue/Course Code (Java)/05-Perfect-Squares/src/Solution3.java
@@ -4,7 +4,19 @@ import javafx.util.Pair;
 // 279. Perfect Squares
 // https://leetcode.com/problems/perfect-squares/description/
 // 进一步优化
-//
+/* 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+
+示例 1:
+
+输入: n = 12
+输出: 3 
+解释: 12 = 4 + 4 + 4.
+示例 2:
+
+输入: n = 13
+输出: 2
+解释: 13 = 4 + 9.
+ */
 // 时间复杂度: O(n)
 // 空间复杂度: O(n)
 public class Solution3 {
