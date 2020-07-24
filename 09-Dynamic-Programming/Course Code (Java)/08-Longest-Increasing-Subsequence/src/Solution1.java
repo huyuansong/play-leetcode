c838be7bec520bd50c50244626769cf481f9c2ba
huyuansong <2455428161@qq.com>
2020-07-24 18:55:42 +0800

    动态规划解决最长上升子序列问题

--- 09-Dynamic-Programming/Course Code (Java)/08-Longest-Increasing-Subsequence/src/Solution1.java
+++ 09-Dynamic-Programming/Course Code (Java)/08-Longest-Increasing-Subsequence/src/Solution1.java
@@ -2,6 +2,21 @@ import java.util.Arrays;
 
 /// 300. Longest Increasing Subsequence
 /// https://leetcode.com/problems/longest-increasing-subsequence/description/
+/* 
+
+给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+示例:
+
+输入: [10,9,2,5,3,7,101,18]
+输出: 4 
+解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+说明:
+
+可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+你算法的时间复杂度应该为 O(n2) 。
+进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+ */
 /// 记忆化搜索
 /// 时间复杂度: O(n^2)
 /// 空间复杂度: O(n)
@@ -14,11 +29,11 @@ public class Solution1 {
         if(nums.length == 0)
             return 0;
 
-        memo = new int[nums.length];
+        memo = new int[nums.length]; 
         Arrays.fill(memo, -1);
-        int res = 1;
-        for(int i = 0 ; i < nums.length ; i ++)
-            res = Math.max(res, getMaxLength(nums, i));
+        int res = 1;  // 上升的子串最短也能有1个，就是自身
+        for(int i = 0 ; i < nums.length ; i ++)         
+            res = Math.max(res, getMaxLength(nums, i));  // 和之前的长度 res 比， 新的子串的长度 谁更长
 
         return res;
     }
@@ -26,7 +41,7 @@ public class Solution1 {
     // 以 nums[index] 为结尾的最长上升子序列的长度
     private int getMaxLength(int[] nums, int index){
 
-        if(memo[index] != -1)
+        if(memo[index] != -1)    
             return memo[index];
 
         int res = 1;
