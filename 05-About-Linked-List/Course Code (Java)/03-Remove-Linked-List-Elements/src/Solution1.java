f190ad9926148c08bb306e30a34b149e9635582f
huyuansong <2455428161@qq.com>
2020-07-27 15:27:27 +0800

    不使用虚拟头结点移除链表的元素

--- 05-About-Linked-List/Course Code (Java)/03-Remove-Linked-List-Elements/src/Solution1.java
+++ 05-About-Linked-List/Course Code (Java)/03-Remove-Linked-List-Elements/src/Solution1.java
@@ -5,11 +5,11 @@
 // 空间复杂度: O(1)
 public class Solution1 {
 
-    public ListNode removeElements(ListNode head, int val) {
+    public ListNode removeElements(ListNode head, int val) {  // 返回值为删除节点之后的链表的头节点
 
         // 需要对头结点进行特殊处理，头节点存在并且要移除的元素就是头节点
-        while(head != null && head.val == val){         //关于头节点的单独处理，如果整个链表都是相同的元素，且每次都删除头节点，相当于删除整个链表
-            ListNode node = head;                       //因此需要while循环在先，if头节点判空在while的后面执行  这里是一很大的坑
+        while(head != null && head.val == val){  //关于头节点的单独处理，如果整个链表都是相同的元素，且每次都删除头节点，相当于删除整个链表
+            ListNode node = head;                //因此需要while循环在先，if头节点判空在while的后面执行  这里是一很大的坑
             head = head.next;
         }
 
@@ -24,7 +24,7 @@ public class Solution1 {
                 delNode = null; //告诉JVM可以回收掉了
             }
             else
-                cur = cur.next;
+                cur = cur.next;   // 指针 cur 变量，向后移动
         }
 
         return head;
