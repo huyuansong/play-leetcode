e74baa8775f143281d531a6e8004ff0b068af30a
huyuansong <2455428161@qq.com>
2020-07-28 11:13:08 +0800

    描述代码语义

--- 09-Dynamic-Programming/Course Code (Java)/04-House-Robber/src/Solution4.java
+++ 09-Dynamic-Programming/Course Code (Java)/04-House-Robber/src/Solution4.java
@@ -14,11 +14,11 @@ public class Solution4 {
         // memo[i] 表示考虑抢劫 nums[0...i] 所能获得的最大收益
         int[] memo = new int[nums.length];
         memo[0] = nums[0];
-        for(int i = 1 ; i < n ; i ++)
+        for(int i = 1 ; i < n ; i ++)  // 抢劫[0,i]范围的收益
             // 求memo[i]
-            for (int j = i; j >= 0; j--)
+            for (int j = i; j >= 0; j--) // i 这个位置抢不抢需要考虑，后面的抢不抢也需要考虑
                 memo[i] = Math.max(memo[i], //memo[i]第一轮为默认值0，后来每一轮和前一轮的结果比较，选出最大值
-                                   nums[j] + (j - 2 >= 0 ? memo[j - 2] : 0));
+                                   nums[j] + (j - 2 >= 0 ? memo[j - 2] : 0)); // j第一个位置默认是抢的，j--表示j位置不抢
 
         return memo[n-1];
     }
